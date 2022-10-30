c3cd735199b96591b9915cd04fc1311b6976cee4
Vyasdev <vyasdev2002@yahoo.com>
2022-10-30 22:18:09 +0530

    Create YouTube_HomePage_NoDistraction.user.js

---
+++ YouTube_HomePage_NoDistraction.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         YouTube-HomePage-NoDistraction
+// @namespace    https://github.com/Vyasdev217
+// @version      1.0
+// @description  Distracted by video suggestions on youtube homepage? This will solve it!
+// @author       LoneWold
+// @match        https://www.youtube.com/
+// @grant        none
+// @license MIT
+// ==/UserScript==
+
+    document.getElementById("content").addEventListener("click",function(){
+        document.getElementById('page-manager').remove();
+    },false);
