9cd1e2b08b1be6d155133859743d22c07d145786
Vyasdev <vyasdev2002@yahoo.com>
2022-10-30 22:19:08 +0530

    Update YouTube_HomePage_NoDistraction.user.js
    Correction in author name

--- YouTube_HomePage_NoDistraction.user.js
+++ YouTube_HomePage_NoDistraction.user.js
@@ -3,12 +3,12 @@
 // @namespace    https://github.com/Vyasdev217
 // @version      1.0
 // @description  Distracted by video suggestions on youtube homepage? This will solve it!
-// @author       LoneWold
+// @author       Lone Wolf
 // @match        https://www.youtube.com/
 // @grant        none
 // @license MIT
 // ==/UserScript==
 
-    document.getElementById("content").addEventListener("click",function(){
-        document.getElementById('page-manager').remove();
-    },false);
+document.getElementById("content").addEventListener("click",function(){
+    document.getElementById('page-manager').remove();
+},false);
